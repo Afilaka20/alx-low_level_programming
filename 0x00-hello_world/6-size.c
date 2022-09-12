20067ad2b28bcbb670d9107c3449ebf716c10fdf
Afilaka20 <Afilakaayodele20@gmail.com>
2022-09-12 09:23:51 -0700

    c task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 
 	printf("size of a int: %ld\n", sizeof(i));
 	printf("size of a char: %ld\n", sizeof(c));
-	printf("size of a long int: %ld\n", sizeof(lli));
+	printf("size of a long int: %ld\n", sizeof(li));
 	printf("size of a long long int: %ld\n", sizeof(lli));
 	printf("size of a float:%ld\n", sizeof(f));
 	return (0);

0b228bb5df89ed046ee1f3fabdb82a9c78006cf5
Afilaka20 <Afilakaayodele20@gmail.com>
2022-09-13 19:18:26 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: using the main function
+ * main -  prints the alphabet, in lowercase, followed by a new line
+ * Return: always 0 (success)
+ */
+
+;void print_alphabet(void);
+{
+	/* print_alphabets a-z */
+	_Putchar ('\n');
+	return (0);
+}
